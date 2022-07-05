558a35b0f708e104ba3e785fc4e44c1a233017ff
jaimefernandez1889 <57432507+jaimefernandez1889@users.noreply.github.com>
2022-07-05 17:17:12 +0200

    Note list with Type enum class

--- src/jvmMain/kotlin/Note.kt
+++ src/jvmMain/kotlin/Note.kt
@@ -1,1 +1,18 @@
-data class Note(val title: String, val description: String)
+data class Note(val title: String, val description: String, val type: Type){
+    enum class Type {
+        TEXT, AUDIO, VIDEO
+    }
+}
+
+val noteList = listOf(
+    Note("t1", "d1", Note.Type.TEXT),
+    Note("t2", "d2", Note.Type.AUDIO),
+    Note("t3", "d3", Note.Type.TEXT),
+    Note("t4", "d4", Note.Type.TEXT),
+    Note("t5", "d5", Note.Type.AUDIO),
+    Note("t6", "d6", Note.Type.TEXT),
+    Note("t7", "d7", Note.Type.TEXT),
+    Note("t8", "d8", Note.Type.AUDIO),
+    Note("t9", "d9", Note.Type.TEXT),
+    Note("t10", "d10", Note.Type.TEXT),
+)
